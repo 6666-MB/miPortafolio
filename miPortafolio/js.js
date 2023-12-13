254f51b431674a89026cbe685ad4c9b3751a0848
Marcelo Bogado <124157210+6666-MB@users.noreply.github.com>
2023-12-13 20:43:58 -0300

    Update js.js

--- miPortafolio/js.js
+++ miPortafolio/js.js
@@ -1,11 +1,1 @@
 // script.js
-
-const form = document.querySelector('.contact form');
-
-form.addEventListener('submit', (evento) => {
-    evento.preventDefault();
-    const nombre = document.querySelector('#name').value;
-    const email = document.querySelector('#email').value;
-    const mensaje = document.querySelector('#message').value;
-    alert(`Se ha recibido tu mensaje. ¡Gracias ${nombre}!`);
-});
